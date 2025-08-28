0298680cfbdfa035d1d1a81e32cf5a9ab779f3e5
saqib diar <saqibdiar.uet@gmail.com>
2025-08-28 18:43:57 +0500

    feat: implement user registration with password hashing and JWT token generation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,37 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
 
 const register = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
     }
+
+    const user = await User.create({
+      name,
+      email,
+      password
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const login = async (req, res) => {
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,6 +26,17 @@ const userSchema = new mongoose.Schema({
 }, {
   timestamps: true
 });
+
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 
 module.exports = mongoose.model('User', userSchema);
