9d9b99e9de084c523d3fbc239877e22aa485ddd3
saqib diar <saqibdiar.uet@gmail.com>
2025-08-28 19:21:21 +0500

    feat: add auth middleware and implement user login and get profile

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,19 +35,33 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user && (await user.correctPassword(password, user.password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
     }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const getProfile = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
+  try {
+    res.json(req.user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
+
 
 module.exports = { register, login, getProfile };
