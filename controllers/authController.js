bf179b9c8a7ed8dea2d79cef4e31973fe27d928f
Saqib Diar <saqib.diar@bcp.com.pk>
2025-08-28 18:16:26 +0500

    feat: add auth and task routes with controller stubs

---
+++ controllers/authController.js
@@ -0,0 +1,28 @@
+const User = require('../models/user');
+
+
+const register = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const getProfile = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+module.exports = { register, login, getProfile };
---
+++ controllers/taskController.js
@@ -0,0 +1,35 @@
+const Task = require('../models/task');
+
+const getTasks = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const createTask = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const updateTask = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const deleteTask = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+module.exports = { getTasks, createTask, updateTask, deleteTask };
